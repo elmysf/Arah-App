48757f280740aa99ecf0cda6a07d79eb7fb10416
elmysf <elmysufiandy09@gmail.com>
2023-11-18 09:15:48 +0700

    Initial Commit

---
+++ Arah/ArahApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArahApp.swift
+//  Arah
+//
+//  Created by Sufiandy Elmy on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ArahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
